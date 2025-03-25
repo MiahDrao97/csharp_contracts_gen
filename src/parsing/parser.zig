bf75e87631d9b7d775afd702fdae2883e0dcd85e
Jeremiah D. Oard <germoard@gmail.com>
2025-03-25 11:09:30 -0600

    successfully parsing node array from tokens

--- src/parsing/parser.zig
+++ src/parsing/parser.zig
@@ -49,16 +49,17 @@ fn parseObj(
     try tokens.expectIndentLevel(indent_depth);
     while (tokens.next()) |tok| {
         defer self.tok_idx += 1;
+        log.debug("Next token while parsing object: '{s}' (tokens[{d}])", .{ tok.asString(), self.tok_idx });
         switch (tok) {
             .string => |s| {
                 if (key == null) {
                     key = s;
                 } else if (expecting_newline_or_eof) {
-                    log.err("Expecting newline or EOF, but found {s} (token[{d}])", .{ s, self.tok_idx });
+                    log.err("Expecting newline or EOF, but found string token '{s}' (token[{d}])", .{ s, self.tok_idx });
                     dumpNodeMap(node);
                     return error.UnexpectedToken;
                 } else if (!colon_found) {
-                    log.err("Expecting colon after key, but found {s} (token[{d}])", .{ s, self.tok_idx });
+                    log.err("Expecting colon after key, but found string token '{s}' (token[{d}])", .{ s, self.tok_idx });
                     return error.UnexpectedToken;
                 } else {
                     // key-value
@@ -77,6 +78,7 @@ fn parseObj(
                         },
                     };
                     colon_found = true;
+                    log.debug("Colon found (tokens[{d}])", .{self.tok_idx});
                 } else if (key != null and colon_found) {
                     tok.expectSyntax(.newline) catch |err| switch (err) {
                         error.UnexpectedToken => {
@@ -88,6 +90,7 @@ fn parseObj(
                     key = null;
                     colon_found = false;
                     expecting_newline_or_eof = true;
+                    log.debug("Finished parsing array (tokens[{d}])", .{self.tok_idx});
                 } else if (expecting_newline_or_eof) {
                     // EOF is not a parsable token and would not be returned from `TokenIterator`
                     tok.expectSyntax(.newline) catch |err| switch (err) {
@@ -96,6 +99,7 @@ fn parseObj(
                             return err;
                         },
                     };
+                    expecting_newline_or_eof = false;
                 } else {
                     log.err("Found unexpected syntax {s}. Was expecting string token (token[{d}])", .{ @tagName(syn), self.tok_idx });
                     return error.UnexpectedToken;
@@ -134,9 +138,7 @@ fn parseObjOrArray(
                         log.err("Expected dash syntax but found {s} (token[{d}]). Next token: {s}", .{
                             @tagName(syn),
                             self.tok_idx,
-                            blk: {
-                                if (tokens.peek()) |t| break :blk t.asString() else break :blk "<EOF>";
-                            },
+                            if (tokens.peek()) |t| t.asString() else "<EOF>",
                         });
                         return err;
                     }
@@ -299,7 +301,6 @@ test "parse simple object" {
     try testing.expectEqualStrings("value", parsed.root.get("key").?.asValue().?);
 }
 test "parse array" {
-    testing.log_level = .debug;
     // arr:
     //   - item1
     //   - item2
